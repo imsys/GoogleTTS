47f401e9dd625a79502e01a2335b248d7f16c635
Arthur Helfstein Fragoso <arthur@life.net.br>
2011-07-12 19:21:02 -0300

    ### from v0.0.1 to v0.0.9 - Sunday, 16 January 2011 #### - Fixed a bug to be able to work on Windows in the Anki 1.2 - Special characters are working just awesome!!!! TRY IT!!! Thanks to Reimar Döffinger of the Mplayer Team who found out that Google requires us to set the arg -user-agent 'Mozilla/5.0' - Added compatibility with Anki 1.1.1 - Now it's possible to disable the question/answer TTS by commenting the line - enable/disable Mplayer subprocess on MS Windows (for compatibility reasons) - Fixed a bug on MS Windows that used to cut the ending of a speech. You also need to update Mplayer - Fixed a charset problem in MS Windows - Fix a mistake I made in the last update. now it's possible to enable/disable the auto-reading - Added the option to GoogleTTS read automatically the question and/or the answer - Fixed a bug on MS Windows that used to cut the ending of a speech. - Not really - Now you can choose the field to be read. - Code better structured - Hide the mplayer console in MS Windows - it won't read the [sound:*] tags of Anki - Code better s### from v0.0.1 to v0.0.9 - Sunday, 16 January 2011 #### - Fixed a bug to be able to work on Windows in the Anki 1.2 - Special characters are working just awesome!!!! TRY IT!!! Thanks to Reimar Döffinger of the Mplayer Team who found out that Google requires us to set the arg -user-agent 'Mozilla/5.0' - Added compatibility with Anki 1.1.1 - Now it's possible to disable the question/answer TTS by commenting the line - enable/disable Mplayer subprocess on MS Windows (for compatibility reasons) - Fixed a bug on MS Windows that used to cut the ending of a speech. You also need to update Mplayer - Fixed a charset problem in MS Windows - Fix a mistake I made in the last update. now it's possible to enable/disable the auto-reading - Added the option to GoogleTTS read automatically the question and/or the answer - Fixed a bug on MS Windows that used to cut the ending of a speech. - Not really - Now you can choose the field to be read. - Code better structured - Hide the mplayer console in MS Windows - it won't read the [sound:*] tags of Anki

--- GoogleTTS.py
+++ GoogleTTS.py
@@ -5,48 +5,78 @@
 # License: GNU GPL, version 3 or later; http://www.gnu.org/copyleft/gpl.html
 #
 #   GoogleTTS plugin
-
+#
+#   Any problems, comments, please post in this thread:  (or email me)
+#
+#   http://groups.google.com/group/ankisrs/browse_thread/thread/98177e2770659b31
+#
+#  Edited on Sunday, 16 January 2:23:46 p.m.(GMT + 0:00)
+#  
 ########################### Settings #######################################
 
 from PyQt4.QtCore import *
-# the KEY to press to get the answer pronounced
+TTS_read_field = {}
+
+
+# Key to get the Answer pronounced
 TTS_KEY_A=Qt.Key_F4
-# the KEY to press to get the question pronounced
+
+# Key to get the question pronounced
 TTS_KEY_Q=Qt.Key_F3
 
-#The player that will be executed by the Operation System
-#in might work in windows I didn't try, you probably needs the full pach C:/..../mplayer.exe
-TTS_player = 'mplayer'
+
+# Option to automatically recite questions as they appear
+automaticQuestions = False
+#automaticQuestions = True
+
+# Option to automatically recite answers as they appear
+automaticAnswers = False
+#automaticAnswers = True
+
+
+# Keys to get the fields pronounced, case sensitive
+# uncomment and change in a way that works for you,
+# you can add as many as you want
+# examples:
+#TTS_read_field['Field Name'] =  Qt.Key_F9
+#TTS_read_field['Front'] =  Qt.Key_F5
+#TTS_read_field['Back'] = Qt.Key_F6
+#TTS_read_field['Reading'] =  Qt.Key_F7
+#TTS_read_field['Text'] = Qt.Key_F8
+#all the avaliable keys are in http://doc.trolltech.com/qtjambi-4.4/html/com/trolltech/qt/core/Qt.Key.html
+
+
 #Language code
 TTS_language = 'en'
+
 
 """
 Supported Languages
 
-Good:
-en - English
-fr - French
-de - German
-ht - Haitian Creole
-hi - Hindi
-it - Italian
-es - Spanish
-
-Wierd robotic voices:
 af - Afrikaans 
 sq - Albanian
+ar - Arabic
 hy - Armenian
 ca - Catalan
-zh - Chinese - only pinyin with numbers (ni3hao3)
+zh - Chinese
 hr - Croatian
 cs - Czech
 da - Danish
 nl - Dutch
+en - English
 fi - Finnish
+fr - French
+de - German
 el - Greek
+ht - Haitian Creole
+hi - Hindi
 hu - Hungarian
 is - Icelandic
 id - Indonesian
+it - Italian
+ja - Japanese
+ko - Korean
+la - Latin
 lv - Latvian
 mk - Macedonian
 no - Norwegian
@@ -56,6 +86,7 @@ ro - Romanian
 ru - Russian
 sr - Serbian
 sk - Slovak
+es - Spanish
 sw - Swahili
 sv - Swedish
 tr - Turkish
@@ -65,22 +96,58 @@ cy - Welsh
 
 
 ######################### End of Settings ##################################
-import os, subprocess
+import os, subprocess, re, sys
 from ankiqt import mw
-from ankiqt.ui import utils
+from anki import sound
 from anki.utils import stripHTML
-from subprocess import Popen
+from subprocess import Popen, PIPE, STDOUT
+from ankiqt.ui import view
+from anki.hooks import wrap
 
+# mplayer for windows
+if subprocess.mswindows:
+	dir = os.path.dirname(os.path.abspath(sys.argv[0]))
+	os.environ['PATH'] += ";" + dir
+	si = subprocess.STARTUPINFO()
+	try:
+		si.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+	except:
+		# python2.7+
+		si.dwFlags |= subprocess._subprocess.STARTF_USESHOWWINDOW
+
+def TTS_read(text):
+	if text:
+		address = 'http://translate.google.com/translate_tts?tl='+TTS_language+'&q="'+ re.sub("\[sound:.*?\]", "", stripHTML(text).encode('utf-8')) +'"' 
+		if subprocess.mswindows:
+			subprocess.Popen(['mplayer.exe', '-ao', 'win32', '-slave', '-user-agent', "'Mozilla/5.0'", address], startupinfo=si, stdin=PIPE, stdout=PIPE, stderr=STDOUT).communicate()
+                else:
+			subprocess.Popen(['mplayer', '-slave', '-user-agent', "'Mozilla/5.0'", address], stdin=PIPE, stdout=PIPE, stderr=STDOUT)
+
+## Check pressed key
 def newKeyPressEvent(evt):
+    pkey = evt.key()
     if (mw.state == 'showAnswer' or mw.state == 'showQuestion'):
-	if (evt.key() == TTS_KEY_Q):
-		subprocess.Popen([TTS_player, 'http://translate.google.com/translate_tts?tl='+TTS_language+'&q="'+ stripHTML(mw.currentCard.question) +'"'])
-	elif (mw.state=='showAnswer' and evt.key() == TTS_KEY_A):
-		subprocess.Popen([TTS_player, 'http://translate.google.com/translate_tts?tl='+TTS_language+'&q="'+ stripHTML(mw.currentCard.answer) +'"'])
+	if (pkey == TTS_KEY_Q):
+		TTS_read(mw.currentCard.question)
+	elif (mw.state=='showAnswer' and pkey == TTS_KEY_A):
+		TTS_read(mw.currentCard.answer)
+	else:
+		for key in TTS_read_field:
+			if TTS_read_field[key] == pkey:
+				TTS_read(mw.currentCard.fact.get(key, 0))
+				break
 	evt.accept()
     return oldEventHandler(evt)
 
+def GTTSredisplay(self):
+        if (mw.state == 'showQuestion' and automaticQuestions) :
+		TTS_read(mw.currentCard.question)
+	elif (mw.state=='showAnswer' and automaticAnswers) :
+        	TTS_read(mw.currentCard.answer)
+
 oldEventHandler = mw.keyPressEvent
 mw.keyPressEvent = newKeyPressEvent
+view.View.redisplay = wrap(view.View.redisplay, GTTSredisplay,"after")
+
 
 mw.registerPlugin("Google TTS", 0)
